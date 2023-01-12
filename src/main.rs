08428101c1d0f40385634926d7391ea305f84538
Ante Perisic <perisic.ante@gmail.com>
2023-01-12 12:53:35 +0100

    print struct stack address

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,18 @@ struct Point(i32,i32);
 
 fn add(p1: &Point, p2: &Point) -> Point {
 
-    Point(p1.0 + p2.0, p1.1 + p2.1)
+    let p = Point(p1.0 + p2.0, p1.1 + p2.1);
+    //print struct stack address 
+    println!("&p.0: {:p}", &p.0);
+    p
 }
 fn main() {
 
     let p1 = Point(4,9);
     let p2 = Point(2,12);
     let p3 = add(&p1,&p2);
-    println!("{p1:?} + {p2:?} = {p3:?}")
+    println!("{p1:?} + {p2:?} = {p3:?}");
+    println!("&p3.0. {:p}", &p3.0);
 
 
 }
