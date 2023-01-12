ee18f67f5b8f769fb065b8f3c7deeb401cd175ea
Ante Perisic <perisic.ante@gmail.com>
2023-01-12 13:25:53 +0100

    new

---
+++ src/main2.rs
@@ -0,0 +1,31 @@
+
+#[derive(Debug)]
+struct Point(i32,i32);
+
+fn add(p1: &Point, p2: &Point) -> Point {
+
+    let p = Point(p1.0 + p2.0, p1.1 + p2.1);
+    //print struct stack address 
+    println!("temp struct stack address (inside function)->: {:p}", &p.0);
+    p
+}
+fn main() {
+
+    let p1 = Point(4,9);
+    let p2 = Point(2,12);
+    let p3 = add(&p1,&p2);
+    println!("{p1:?} + {p2:?} = {p3:?}");
+    println!("p3 stack address-> {:p}", &p3.0);
+
+    let mut a = 10;
+    let b:&i32 = &a;
+    println!("{b}");
+    {
+        let c: &mut i32  = &mut a;
+        *c = 29; 
+    }
+
+    
+    println!("{a}");
+    
+}
